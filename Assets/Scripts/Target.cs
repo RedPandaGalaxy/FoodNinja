1f83aa001f25d75bd840cf7d0093c55f4842ca52
RedPandaGalaxy <61902899+RedPandaFlame@users.noreply.github.com>
2020-10-21 12:21:28 +0200

    Adding project files to repo

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float minSpeed = 12f;
+    public float maxSpeed = 16f;
+
+    public float maxTorque = 10f;
+
+    public float xRange = 4f;
+    public float ySpawnPos = -6f;
+
+    public int pointValue;
+    private Rigidbody targetRb;
+
+    private GameManager gameManager;
+
+    public ParticleSystem explosionParticle;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        targetRb = GetComponent<Rigidbody>();
+        targetRb.AddForce(RandomUpwardForce(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
+
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    Vector3 RandomUpwardForce()
+    {
+        float randomSpeed = Random.Range(minSpeed, maxSpeed);
+        return Vector3.up * randomSpeed;
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        float randomXPos = Random.Range(-xRange, xRange);
+        return new Vector3(randomXPos, ySpawnPos);
+    }
+
+    float RandomTorque()
+    {
+        float randomTorque = Random.Range(0, maxTorque);
+        return randomTorque;
+    }
+
+    private void OnMouseDown()
+    {
+        if(gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        if(!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+    }
+}
